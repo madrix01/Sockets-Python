35017acfcdfd79ae4ebe802a47c83c027c384df2
madrix01 <shlokcpatel2001@gmail.com>
2020-07-26 01:23:08 +0530

    SC

--- client.py
+++ client.py
@@ -1,8 +1,27 @@
 import socket
 
+HEADERSIZE = 10
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 6969))
 
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+while True:
+
+    full_msg = ""
+    new_msg = True
+
+    while True:
+        msg = s.recv(16) #message recieved
+
+        if new_msg:
+            print(f"New message {msg[:HEADERSIZE]}")
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
+        full_msg += msg.decode("utf-8")
+
+        if len(full_msg) - HEADERSIZE == msglen:
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ""
+    print(full_msg)
+
  
--- server.py
+++ server.py
@@ -1,4 +1,11 @@
 import socket
+import time
+
+HEADERSIZE = 10
+
+msg = "Welcome to the header"
+print(f'{len(msg):<10}' + msg)
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 6969))
@@ -9,4 +16,9 @@ s.listen(5)
 while True:
     clientSocket, adress = s.accept()
     print(f"Connected {adress}")
-    clientSocket.send(bytes("Welcome to the server", "utf-8"))
+
+    while True:
+        time.sleep(3)
+        msg = f"The time is {time.time()}"
+        msg = f'{len(msg):<{HEADERSIZE}}' + msg
+        clientSocket.send(bytes(msg, "utf-8"))
